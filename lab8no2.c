817709172f13f056dd307e45bdc56c8734746a01
6704062611239 <s6704062611239@email.kmutnb.ac.th>
2026-02-10 14:54:29 +0700

    show all score

---
+++ lab8no2.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int checkscore(char std[]);
+int student;
+
+int main() {
+    int i, j;
+    char s[100][100];
+	char ans[8][10] = {
+        {'A','B','A','C','C','D','E','E','A','D'},
+        {'D','B','A','B','C','A','E','E','A','D'},
+        {'E','D','D','A','C','B','E','E','A','D'},
+        {'C','B','A','E','D','C','E','E','A','D'},
+        {'A','B','D','C','C','D','E','E','A','D'},
+        {'B','B','E','C','C','D','E','E','A','D'},
+        {'B','B','A','C','C','D','E','E','A','D'},
+        {'E','B','E','C','C','D','E','E','A','D'}};
+
+    for(i = 0; i < 8; i++) {
+        for(j = 0; j < 10; j++) {
+            s[i][j] = ans[i][j];
+        }
+
+        student = i;
+        printf("std %d => %d\n", (i+1), checkscore(s));
+    }
+}
+
+int checkscore(char std[]) {
+    char keys[10] = {'D','B','D','C','C','D','A','E','A','D'};
+    int i, score = 0;
+
+    for(i = 0; i < 10; i++) {
+        if(std[student * 100 + i] == keys[i])
+            score++;
+    }
+
+    return score;
+}
+
